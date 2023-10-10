20b01213262d8ec697f930c9fc7c5bcc5ec4bbd8
tarmsrch159 <147044492+tarmsrch159@users.noreply.github.com>
2023-10-10 15:25:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ const token_Line = "RAl3CtmN2KGt6eu5g7ZvTdtpA2J1VygIzcnyhiP7VXt";
 
 //Variable for conection to database
 const db = mysql.createConnection({
-  host: "bqmn5uzoqbl28n6wo1ug-mysql.services.clever-cloud.com",
-  user: "ui43my666qg92sci",
-  password: "rlDPwE0lRpJ2c9P3e2aI",
-  database: "bqmn5uzoqbl28n6wo1ug",
+  host: "sql12.freesqldatabase.com",
+  user: "sql12650939",
+  password: "v7CNHS9uKM",
+  database: "sql12650939",
 });
 
 //Conection to database

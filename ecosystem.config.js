14eea743851a2ac1b7bfd0890a277612b3dff0ef
tarmsrch159 <tarmsrch159@gmail.com>
2023-11-16 13:32:30 +0700

    'commit'

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       host : '13.228.225.19',
       ref  : 'origin/master',
       repo : 'git@github.com:tarmsrch159/cloud_server_2.git',
-      path : '/var/www/myApp',
+      path : '/var/www/cloud_server_2.git',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
